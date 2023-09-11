920b5bdb323c826b2678cf552ef4b795ebc41e35
Rajani Navoda <88067036+123Rajani@users.noreply.github.com>
2023-09-11 20:07:15 +0530

    Implement caching mechanism

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
     apiUrl: 'http://api.openweathermap.org/data/2.5/group?id=',
-    apiKey: '2440935d94ffa4d8dd71620f0a022a2e'
+    apiKey: '5cc0ef0d979320acc7b501285e0de769'
 };
